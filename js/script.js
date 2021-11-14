c284609eeadafd69ef3765e2c7ab986ee531e9f0
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-11-14 20:08:21 +0200

    Add operate() function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-function add(a, b) {
-    return a + b;
+function add(number1, number2) {
+    return number1 + number2;
 }
 
-function subtract(a, b) {
-    return a - b;
+function subtract(number1, number2) {
+    return number1 - number2;
 }
 
-function multiply(a, b) {
-    return a * b;
+function multiply(number1, number2) {
+    return number1 * number2;
 }
 
-function divide(a, b) {
-    return a / b;
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+// The function operate takes an arithmetic operator and two numbers,
+// and performs an operation (addition, subtraction, ...) on those two numbers
+function operate(operator, number1, number2) {
+    if (operator === "+") {
+        return add(number1, number2);
+    } else if (operator === "-") {
+        return subtract(number1, number2);
+    } else if (operator === "x") {
+        return multiply(number1, number2);
+    } else if (operator === "/") {
+        return divide(number1, number2);
+    }
 }
