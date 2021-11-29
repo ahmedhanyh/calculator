2b4c487c3016ef3cc9f457b62663673aa30d75f0
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-11-29 18:17:43 +0200

    Made the calculator functional
    clicking on buttons displays the values on the calculator's display and the calculations are done correctly

--- js/script.js
+++ js/script.js
@@ -27,3 +27,85 @@ function operate(operator, number1, number2) {
         return divide(number1, number2);
     }
 }
+
+const displayOperationNode = document.querySelector("#operation");
+const displayResultNode = document.querySelector("#result");
+
+let previousNumber = ""
+  , currentNumber = ""
+  , currentOperator = "";
+
+function isNumber(str) {
+    const strToNumber = Number(str);
+    return String(strToNumber) === str;
+}
+
+function displayNumber(event) {
+    if (event.target.nodeName !== "BUTTON") return;
+    if (!isNumber(event.target.textContent)) return;
+    
+    currentNumber += event.target.textContent;
+    displayOperationNode.textContent += event.target.textContent;
+}
+
+function displayResult(result) {
+    displayResultNode.textContent = result;
+}
+
+function displayOperator(event) {
+    if (event.target.nodeName !== "BUTTON") return;
+    if (isNumber(event.target.textContent)) return;
+    if (event.target.textContent === "C" || event.target.textContent === "=") return;
+
+    if (!previousNumber) {
+        previousNumber = currentNumber;
+    } else {
+        let result = operate(currentOperator, +previousNumber, +currentNumber);
+        if (result === Infinity) {
+            displayResult("ERROR: DIVISION BY ZERO");
+            return;
+        }
+        result = Math.round((result + Number.EPSILON) * 100) / 100;
+        previousNumber = result;
+        displayResult(result);
+    }
+
+    // let result = !previousNumber ? currentNumber
+    //             : operate(currentOperator, +previousNumber, +currentNumber);
+    
+    // previousNumber = result;
+    // displayResult(result);
+
+    currentNumber = "";
+    currentOperator = event.target.textContent.trim();
+    displayOperationNode.textContent += event.target.textContent;
+}
+
+function evaluateOperation() {
+    if (currentNumber === "") {
+        displayResult("ERROR");
+        return;
+    }
+    const result = operate(currentOperator, +previousNumber, +currentNumber);
+    displayResult(result);
+}
+
+function clearDisplay() {
+    previousNumber = "";
+    currentNumber = "";
+    currentOperator = "";
+    displayOperationNode.textContent = "";
+    displayResultNode.textContent = "";
+}
+
+document.querySelector("#calculator-buttons")
+  .addEventListener("click", displayNumber);
+
+document.querySelector("#calculator-buttons")
+  .addEventListener("click", displayOperator);
+
+document.querySelector("#equal-button")
+  .addEventListener("click", evaluateOperation);
+
+document.querySelector("#clear-button")
+  .addEventListener("click", clearDisplay);
